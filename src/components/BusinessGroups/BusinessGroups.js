c259890f7ccd33b70b6986a748d5198619281a21
BenLirio <benjlirio@gmail.com>
2020-02-23 19:04:47 -0500

    Base busines groups without list

--- src/components/BusinessGroups/BusinessGroups.js
+++ src/components/BusinessGroups/BusinessGroups.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import './BusinessGroups.less'
+import { Typography, Divider } from 'antd'
+import BusinessGroupsList from './BusinessGroupsList/BusinessGroupsList'
+
+const { Text } = Typography
 
 const BusinessGroups = () => {
   return (
     <div className={'BusinessGroups-root'}>
-      <div className={'BusinessGroups-container'}></div>
+      <div className={'BusinessGroups-container'}>
+        <Text strong>Business Group</Text>
+        <Divider className={'BusinessGroups-divider'} />
+        <BusinessGroupsList />
+      </div>
     </div>
   )
 }
---
+++ src/components/BusinessGroups/BusinessGroupsList/BusinessGroupsList.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const BusinessGroupsList = () => {
+  return <div>BusinessGroupsList</div>
+}
+
+export default BusinessGroupsList
