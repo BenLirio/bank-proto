5e7442585eb07536a89b20272e04422407566e99
BenLirio <benjlirio@gmail.com>
2020-02-23 21:43:27 -0500

    Add overflow to table

--- src/components/Workspaces/WorkspaceTable/WorkspaceTable.js
+++ src/components/Workspaces/WorkspaceTable/WorkspaceTable.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Table } from 'antd'
+import { AutoSizer } from 'react-virtualized'
 
 const { Column } = Table
 
@@ -14,9 +15,32 @@ for (let i = 0; i < 100; i++) {
 
 const WorkspaceTable = () => {
   return (
-    <Table dataSource={data} pagination={{ pageSize: 50 }}>
-      <Column title="Name" dataIndex={'name'} key={'id'} />
-    </Table>
+    <div style={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+      <div style={{ background: '#000', height: '100px' }}></div>
+      <div style={{ flex: '1' }}>
+        <AutoSizer disableWidth>
+          {({ height }) => {
+            console.log('height', height)
+            return (
+              <div style={{ height, overflow: 'scroll' }}>
+                <Table dataSource={data} pagination={false} showHeader={false}>
+                  <Column title="Name" dataIndex={'name'} key={'id'} />
+                </Table>
+              </div>
+            )
+          }}
+        </AutoSizer>
+      </div>
+      <div style={{ background: '#000', height: '100px' }}></div>
+    </div>
+    // <AutoSizer disableWidth>
+    //   {({ height }) => {
+    //     return <div style={{ height, background: '#000' }}></div>
+    //   }}
+    // </AutoSizer>
+    // {/* <Table dataSource={data} pagination={{ pageSize: 50 }}>
+    //   <Column title="Name" dataIndex={'name'} key={'id'} />
+    // </Table> */}
   )
 }
 
