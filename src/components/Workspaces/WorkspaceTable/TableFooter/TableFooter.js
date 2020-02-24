c4a438a4dd2e2162cfe7f23a38c63fa6e2c33eab
BenLirio <benjlirio@gmail.com>
2020-02-23 22:45:10 -0500

    Complete all selected

--- src/components/Workspaces/WorkspaceTable/TableFooter/TableFooter.js
+++ src/components/Workspaces/WorkspaceTable/TableFooter/TableFooter.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import './TableFooter.less'
+import { Pagination, Button } from 'antd'
+import { Typography } from 'antd'
+
+const { Text } = Typography
 
 const TableFooter = () => {
-  return <div>TableFooter</div>
+  return (
+    <div className="TableFooter-root">
+      <Text strong className="TableFooter-updateText">
+        Update all selected to:
+      </Text>
+      <Button className="TableFooter-button">90 days</Button>
+      <Button className="TableFooter-button">180 days</Button>
+      <Button className="TableFooter-button">365 days</Button>
+      <div style={{ flex: '1' }}></div>
+      <Pagination />
+    </div>
+  )
 }
 
 export default TableFooter
