9a018bce1a3945fe25a5802a2895721ba70c8ee4
BenLirio <benjlirio@gmail.com>
2020-02-26 11:06:41 -0500

    Add modal to confirm

--- src/components/Workspaces/WorkspaceTable/TableFooter/TableFooter.js
+++ src/components/Workspaces/WorkspaceTable/TableFooter/TableFooter.js
@@ -1,39 +1,47 @@
-import { Button, Typography } from 'antd'
+import { Button, Typography, Modal } from 'antd'
 import React, { useContext } from 'react'
 import Pagination from './Pagination/Pagination'
 import './TableFooter.less'
 import WorkspaceContext from '../workspace-context'
 
 const { Text } = Typography
+const { confirm } = Modal
 
 const TableFooter = () => {
-  const dispatchWorkspaces = useContext(WorkspaceContext)[1]
+  const [workspaces, dispatchWorkspaces] = useContext(WorkspaceContext)
   const setPolicies = policy => {
     dispatchWorkspaces({ type: 'setPolicies', payload: policy })
   }
+  const showConfirm = policy => {
+    const numChecked = workspaces.reduce((acc, { checked }) => {
+      return acc + (checked ? 1 : 0)
+    }, 0)
+    confirm({
+      title: 'Confirm',
+      content: `Change ${numChecked} workspaces to ${policy}`,
+      onCancel() {},
+      onOk() {
+        setPolicies(policy)
+      }
+    })
+  }
+  const buttons = ['90 days', '180 days', '365 days']
   return (
     <div className="TableFooter-root">
       <Text strong className="TableFooter-updateText">
         Update all selected to:
       </Text>
-      <Button
-        className="TableFooter-button"
-        onClick={() => setPolicies('90 days')}
-      >
-        90 days
-      </Button>
-      <Button
-        className="TableFooter-button"
-        onClick={() => setPolicies('180 days')}
-      >
-        180 days
-      </Button>
-      <Button
-        className="TableFooter-button"
-        onClick={() => setPolicies('365 days')}
-      >
-        365 days
-      </Button>
+      {buttons.map(button => {
+        return (
+          <Button
+            key={button}
+            className="TableFooter-button"
+            onClick={() => showConfirm(button)}
+          >
+            {button}
+          </Button>
+        )
+      })}
       <div style={{ flex: '1' }}></div>
       <Pagination />
     </div>
