419b3590092f1324a9f3d34c03bad27002bb4f94
BenLirio <benjlirio@gmail.com>
2020-02-26 09:33:39 -0500

    By text Filter

--- src/components/Workspaces/Filters/SearchFilter/SearchFilter.js
+++ src/components/Workspaces/Filters/SearchFilter/SearchFilter.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Typography, AutoComplete } from 'antd'
+import WorkspaceContext from '../../WorkspaceTable/workspace-context'
 
 const { Text } = Typography
 
 const SearchFilter = () => {
+  const dispatchWorkspaces = useContext(WorkspaceContext)[1]
+  const setFilter = val => {
+    const show = ({ name }) => {
+      return name.includes(val)
+    }
+    setTimeout(() => {
+      dispatchWorkspaces({ type: 'filter', payload: { show, name: 'name' } })
+    }, 0)
+  }
   return (
     <>
       <Text strong>SearchFilter</Text>
-      <AutoComplete />
+      <AutoComplete onChange={val => setFilter(val)} />
     </>
   )
 }
